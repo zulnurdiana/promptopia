1422a8075852d9eac9c67dfbc2139254b71e9e97
zulnurdiana <zulnurdiana@gmail.com>
2023-08-07 12:24:20 +0700

    change main title

--- app/page.jsx
+++ app/page.jsx
@@ -5,14 +5,14 @@ const Home = () => {
     <section>
       <div className="w-full flex-center flex-col">
         <h1 className="head_text text-center">
-            Discover & Share
-            <br className="max-md:hidden" />
-            <span className="orange_gradient text-center">
-                AI-Powered Prompts
-            </span>
+          Discover & Share
+          <br className="max-md:hidden" />
+          <span className="orange_gradient text-center">
+            AI-Powered Prompts
+          </span>
         </h1>
         <p className="desc text-center">
-          Promtopia is open-souce AI Prompting tool for modern world to discover, create and share creative prompt.
+          Promtopia is open-souce AI Prompting tool for modern world to discover, create and share creative prompt with promptopia
         </p>
       </div>
 
